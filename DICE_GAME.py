1acadd61bcd331628bbfc522c07b02fc3104dfd5
Dariusz-Krol <newgenconou@gmail.com>
2022-04-17 15:55:00 +0200

    Create DICE_GAME.py
    Just most simple Dice. Just throw as many times as you want

---
+++ DICE_GAME.py
@@ -0,0 +1,9 @@
+import random
+DICE_1 = random.randint(1 , 6)
+DICE_2 = random.randint(1 , 6)
+print()
+print(f'Result of the first DICE: {DICE_1}')
+print()
+print(f'Result of the second DICE: {DICE_2}')
+print()
+print(f'SUM: {DICE_1 + DICE_2}')
